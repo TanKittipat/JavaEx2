d7692ac2408e436a98d6cdd35aceec39732986fa
TanKittipat <kittipat12tan@gmail.com>
2023-02-10 14:56:40 +0700

    2023/02/10

--- Ex6.java
+++ Ex6.java
@@ -1,13 +1,13 @@
 public class Ex6 {
     public static void main(String[] args) {
-        int answer = 1;
-        for (int i = 0; i <= 100; i++) {
+        double answer = 1;
+        System.out.println("The multiple of even number of 1 to 100 is");
+        for (double i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
                 answer = answer * i;
+                System.out.println(answer);
             }
 
         }
-        System.out.println("The multiple of even number of 1 to 100 is ");
-        System.out.print(answer);
     }
 }
